ad18ed45301a3555c12c87481b7d11da5a0d3f76
unknown <dubey07.abhishek@gmail.com>
2020-04-07 15:45:43 +0530

    config externalization

--- src/Session.scala
+++ src/Session.scala
@@ -1,8 +1,5 @@
-package common
-
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
 import org.apache.spark.sql.SparkSession
+import com.typesafe.config.ConfigFactory
 
 object Session {
   def main(args: Array[String]): Unit = {
