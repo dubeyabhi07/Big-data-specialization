ea022c4db063fd1d0aa8fd4fe53fa4e6784ec6d8
unknown <dubey07.abhishek@gmail.com>
2020-05-09 16:46:31 +0530

    basic streaming

--- build.sbt
+++ build.sbt
@@ -9,6 +9,7 @@ libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-sql" % "2.4.3",
   "org.apache.spark" %% "spark-mllib" % "2.4.3",
   "com.typesafe" % "config" % "1.4.0",
-  "org.scalafx" %% "scalafx" % "12.0.2-R18"
-
+  "org.scalafx" %% "scalafx" % "12.0.2-R18",
+  "org.apache.spark" %% "spark-streaming" % "2.4.3" % "provided",
+  "org.apache.bahir" %% "spark-streaming-twitter" % "2.4.0"
 )
---
+++ src/main/scala/twitter/TwitterTrendingHashtags.scala
@@ -0,0 +1,61 @@
+package twitter
+
+import com.typesafe.config.ConfigFactory
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.streaming.twitter.TwitterUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+object TwitterTrendingHashtags {
+
+  def main(args: Array[String]): Unit = {
+
+    val props = ConfigFactory.load("application.properties")
+    System.setProperty("twitter4j.oauth.consumerKey", props.getString("twitter4j.oauth.consumerKey"))
+    System.setProperty("twitter4j.oauth.consumerSecret", props.getString("twitter4j.oauth.consumerSecret"))
+    System.setProperty("twitter4j.oauth.accessToken", props.getString("twitter4j.oauth.accessToken"))
+    System.setProperty("twitter4j.oauth.accessTokenSecret", props.getString("twitter4j.oauth.accessTokenSecret"))
+
+    val sparkSess = SparkSession.builder()
+      .appName("twitter-trending-hashtags")
+      .master("local[2]")
+      .getOrCreate()
+    val sc = sparkSess.sparkContext
+    val streamingContext = new StreamingContext(sc, Seconds(5))
+    val sparkContext = sparkSess.sparkContext
+    sparkContext.setLogLevel("ERROR")
+
+    val stream = TwitterUtils.createStream(streamingContext, None)
+
+    val hashTags = stream.flatMap(status => status.getText.split(" ")
+      .filter(words => words.startsWith("#")))
+
+    // Get the top hashtags over the previous 60 sec window
+    val topCounts60 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(60))
+      .map { case (topic, count) => (count, topic) }
+      .transform(_.sortByKey(false))
+
+    // Get the top hashtags over the previous 10 sec window
+    val topCounts10 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(10))
+      .map { case (topic, count) => (count, topic) }
+      .transform(_.sortByKey(false))
+
+    // print tweets in the currect DStream
+    // println(stream.print())
+
+    topCounts60.foreachRDD(rdd => {
+      val topList = rdd.take(10)
+      println("\nPopular topics in last 60 seconds (%s total):".format(rdd.count()))
+      topList.foreach { case (count, tag) => println("%s (%s tweets)".format(tag, count)) }
+    })
+    topCounts10.foreachRDD(rdd => {
+      val topList = rdd.take(10)
+      println("\nPopular topics in last 10 seconds (%s total):".format(rdd.count()))
+      topList.foreach { case (count, tag) => println("%s (%s tweets)".format(tag, count)) }
+    })
+
+    streamingContext.start();
+    streamingContext.awaitTermination();
+
+  }
+
+}
